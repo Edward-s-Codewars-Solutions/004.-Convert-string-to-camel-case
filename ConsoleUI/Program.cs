4116aeac7f7cdbfb2df228176822c245569faec1
Edward Zieminski <Edward.Zieminski@gmail.com>
2020-12-25 22:42:34 +0100

    Implemented ConsoleUI demo

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,3 +1,14 @@
 ﻿using KataLibrary;
 using System;
 
+Console.WriteLine("KATA 004: Convert string to camel case\n");
+
+Console.WriteLine("Give me some sentence with dashes or underscores instead of spaces: ");
+string inputSentence = Console.ReadLine();
+
+var convertedSentence = Kata.ToCamelCase(inputSentence);
+Console.WriteLine("Converted sentence: ");
+Console.WriteLine(convertedSentence);
+
+Console.Write("\nPress any key...");
+Console.ReadKey();
