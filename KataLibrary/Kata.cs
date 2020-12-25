962a7f24a91066694b4c61018c4833a828b7bed9
Edward Zieminski <Edward.Zieminski@gmail.com>
2020-12-25 20:41:51 +0100

    Added regex pattern
    The pattern matches dash-letter and underscore letter matches. Letter can be either lower or uppercase. Pattern tested on regex101.com

--- KataLibrary/Kata.cs
+++ KataLibrary/Kata.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 public static class Kata
 {
+    private const string DashLetterRegexPattern = @"[\-\_][a-zA-Z]";
+
     public static string ToCamelCase(string sentence)
     {
-        throw new NotImplementedException();
+        
     }
 }
