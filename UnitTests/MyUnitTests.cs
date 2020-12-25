5e0402ce4d17c63a360e34cfb6d69b831319907f
Edward Zieminski <Edward.Zieminski@gmail.com>
2020-12-25 22:31:28 +0100

    Added namespace UnitTests

--- UnitTests/MyUnitTests.cs
+++ UnitTests/MyUnitTests.cs
@@ -3,31 +3,35 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-[TestFixture]
-public class MyUnitTests
+namespace UnitTests
 {
-    private static IEnumerable<TestCaseData> TestCasesForRetrievingUCaseSecondCharOfMatch
+    [TestFixture]
+    public class MyUnitTests
     {
-        get
+        private static IEnumerable<TestCaseData> TestCasesForRetrievingUCaseSecondCharOfMatch
         {
-            yield return new TestCaseData(StringToMatch("test")).Returns("E");
-            yield return new TestCaseData(StringToMatch("tEst")).Returns("E");
-            yield return new TestCaseData(StringToMatch("-e")).Returns("E");
-            yield return new TestCaseData(StringToMatch("-E")).Returns("E");
-            yield return new TestCaseData(StringToMatch("_e")).Returns("E");
-            yield return new TestCaseData(StringToMatch("_E")).Returns("E");
-            yield return new TestCaseData(StringToMatch("a")).Returns(default(string));
+            get
+            {
+                yield return new TestCaseData(StringToMatch("test")).Returns("E");
+                yield return new TestCaseData(StringToMatch("tEst")).Returns("E");
+                yield return new TestCaseData(StringToMatch("-e")).Returns("E");
+                yield return new TestCaseData(StringToMatch("-E")).Returns("E");
+                yield return new TestCaseData(StringToMatch("_e")).Returns("E");
+                yield return new TestCaseData(StringToMatch("_E")).Returns("E");
+                yield return new TestCaseData(StringToMatch("a")).Returns(default(string));
+            }
+        }
+
+        [Test, TestCaseSource("TestCasesForRetrievingUCaseSecondCharOfMatch")]
+        public string MatchToUCaseSecondChar_returns_second_char_of_the_match(Match match) =>
+            Kata.MatchToUCaseSecondChar(match);
+
+        private static Match StringToMatch(string inputString)
+        {
+            string regexPattern = $".{{{ inputString.Length }}}"; // any char repeated for as many times as inputString's length
+            return Regex
+                .Match(inputString, regexPattern);
         }
     }
 
-    [Test, TestCaseSource("TestCasesForRetrievingUCaseSecondCharOfMatch")]
-    public string MatchToUCaseSecondChar_returns_second_char_of_the_match(Match match) =>
-        Kata.MatchToUCaseSecondChar(match);
-
-    private static Match StringToMatch(string inputString)
-    {
-        string regexPattern = $".{{{ inputString.Length }}}"; // any char repeated for as many times as inputString's length
-        return Regex
-            .Match(inputString, regexPattern);
-    }
 }
--- UnitTests/UnitTestsFromThePortal.cs
+++ UnitTests/UnitTestsFromThePortal.cs
@@ -3,13 +3,16 @@
 using KataLibrary;
 using NUnit.Framework;
 
-[TestFixture]
-public class UnitTestsFromThePortal
+namespace UnitTests
 {
-    [Test]
-    public void KataTests()
+    [TestFixture]
+    public class UnitTestsFromThePortal
     {
-        Assert.AreEqual("theStealthWarrior", Kata.ToCamelCase("the_stealth_warrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
-        Assert.AreEqual("TheStealthWarrior", Kata.ToCamelCase("The-Stealth-Warrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual("theStealthWarrior", Kata.ToCamelCase("the_stealth_warrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
+            Assert.AreEqual("TheStealthWarrior", Kata.ToCamelCase("The-Stealth-Warrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+        }
     }
 }
